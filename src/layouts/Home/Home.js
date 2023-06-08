36a2251686ae2f9035bef7c74235f5712f6ad78d
djshafat <shafatmahmudkhan@gmail.com>
2023-06-08 23:13:44 +0600

    nt

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -18,7 +18,13 @@ import { ProjectSummary } from 'layouts/Home/ProjectSummary';
 import { useEffect, useRef, useState } from 'react';
 import styles from './Home.module.css';
 
-const disciplines = ['Developer', 'Prototyper', 'Animator', 'Illustrator', 'Modder'];
+const disciplines = [
+  'Dreamer',
+  'Coder',
+  'Business Planner',
+  'Web Designer',
+  'WordPress Customizer',
+];
 
 export const Home = () => {
   const [visibleSections, setVisibleSections] = useState([]);
@@ -68,9 +74,8 @@ export const Home = () => {
   return (
     <div className={styles.home}>
       <Meta
-        title="Designer + Developer"
-        description="Design portfolio of Hamish Williams — a product designer working on web & mobile
-          apps with a focus on motion, experience design, and accessibility."
+        title="Shafat Mahmud Khan | Entreprenuer | Dreamer"
+        description="Talks about #business, #solarenergy, #dataanalytics, #webdevelopment, and #entrepreneurship"
       />
       <Intro
         id="intro"
--- src/layouts/Home/Intro.js
+++ src/layouts/Home/Intro.js
@@ -65,11 +65,11 @@ export function Intro({ id, sectionRef, disciplines, scrollIndicatorHidden, ...r
             <DisplacementSphere />
             <header className={styles.text}>
               <h1 className={styles.name} data-visible={visible} id={titleId}>
-                <DecoderText text="Hamish Williams" delay={300} />
+                <DecoderText text="Shafat Mahmud Khan" delay={300} />
               </h1>
               <Heading level={0} as="h2" className={styles.title}>
                 <VisuallyHidden className={styles.label}>
-                  {`Designer + ${introLabel}`}
+                  {`Entreprenuer + ${introLabel}`}
                 </VisuallyHidden>
                 <span aria-hidden className={styles.row}>
                   <span
@@ -77,7 +77,7 @@ export function Intro({ id, sectionRef, disciplines, scrollIndicatorHidden, ...r
                     data-status={status}
                     style={cssProps({ delay: tokens.base.durationXS })}
                   >
-                    Designer
+                    Entreprenuer
                   </span>
                   <span className={styles.line} data-status={status} />
                 </span>
@@ -134,3 +134,4 @@ export function Intro({ id, sectionRef, disciplines, scrollIndicatorHidden, ...r
     </Section>
   );
 }
+export default dynamic (() => Promise.resolve(Intro), {ssr: false});
